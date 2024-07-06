db6a010f9183bea26d30559ee5a13ba9f123258b
Юлия <118264321+Morozova91@users.noreply.github.com>
2024-07-06 16:34:25 +0300

    Add files via upload

---
+++ module_6_2.py
@@ -0,0 +1,78 @@
+# I. Каждый объект Vehicle должен содержать следующие атрибуты объекта:
+# Атрибут owner(str) - владелец транспорта. (владелец может меняться)
+# Атрибут __model(str) - модель (марка) транспорта. (мы не можем менять название модели)
+# Атрибут __engine_power(int) - мощность двигателя. (мы не можем менять мощность двигателя самостоятельно)
+# Атрибут __color(str) - название цвета. (мы не можем менять цвет автомобиля своими руками)
+# А так же атрибут класса:
+# Атрибут класса __COLOR_VARIANTS, в который записан список допустимых цветов для окрашивания. (Цвета написать свои)
+# Каждый объект Vehicle должен содержать следующий методы:
+# Метод get_model - возвращает строку: "Модель: <название модели транспорта>"
+# Метод get_horsepower - возвращает строку: "Мощность двигателя: <мощность>"
+# Метод get_color - возвращает строку: "Цвет: <цвет транспорта>"
+# Метод print_info - распечатывает результаты методов (в том же порядке): get_model, get_horsepower, get_color; а так же владельца в конце в формате "Владелец: <имя>"
+# Метод set_color - принимает аргумент new_color(str), меняет цвет __color на new_color, если он есть в списке __COLOR_VARIANTS, в противном случае выводит на экран надпись: "Нельзя сменить цвет на <новый цвет>".
+# Взаимосвязь методов и скрытых атрибутов:
+# Методы get_model, get_horsepower, get_color находятся в одном классе с соответствующими им атрибутами: __model, __engine_power, __color. Поэтому атрибуты будут доступны для методов.
+# Атрибут класса __COLOR_VARIANTS можно получить обращаясь к нему через объект(self).
+# Проверка в __COLOR_VARIANTS происходит не учитывая регистр ('BLACK' ~ 'black').
+# II. Класс Sedan наследуется от класса Vehicle, а так же содержит следующие атрибуты:
+# Атрибут __PASSENGERS_LIMIT = 5 (в седан может поместиться только 5 пассажиров)
+#
+# Пункты задачи:
+# Создайте классы Vehicle и Sedan.
+# Напишите соответствующие свойства в обоих классах.
+# Не забудьте сделать Sedan наследником класса Vehicle.
+# Создайте объект класса Sedan и проверьте, как работают все его методы, взяты из класса Vehicle.
+
+from termcolor import colored
+
+
+class Vehicle:  # это любой транспорт
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black',
+                        'white']  # атрибут класса, который не переопределить, потому __
+
+    def __init__(self, owner: str, model: str, color: str, engine_power: int):
+
+        self.__model = model
+        self.__color = color
+
+        self.__engine_power = engine_power
+        self.owner = owner
+
+    def get_model(self):
+        return self.__model
+
+    def get_horsepower(self):
+        return self.__engine_power
+
+    def get_color(self):
+        return self.__color
+
+    def set_color(self, new_color: str):
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
+        else:
+            print(f'Нельзя сменить цвет на {new_color}')
+
+    def print_info(self):
+        print("Модель:", self.get_model())
+        print("Мощность:", self.get_horsepower())
+        print("Цвет:", self.get_color())
+        print("Владелец:", self.owner)
+
+
+class Sedan(Vehicle):  #(седан) - наследник класса Vehicle.
+    __PASSENGERS_LIMIT = 5
+
+
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+
+vehicle1.print_info()
+
+vehicle1.set_color('Pink')
+
+vehicle1.set_color('BLACK')
+
+vehicle1.owner = 'Vasyok'
+
+vehicle1.print_info()
